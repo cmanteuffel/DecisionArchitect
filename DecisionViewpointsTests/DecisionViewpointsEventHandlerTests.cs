5b0f8413eac0617739b2e62a7fafa4cd246e72fd
MarkHoekstra_cp <SND\MarkHoekstra_cp>
2014-04-17 17:51:03 +0000

    initial commit of test framework (TFS Integration from 'addin.visualstudio.com (VC)' Id: 37;CreationDate: 2013-02-21 15:54:30 UTC;)((Edited by spyrosikmd@gmail.com))

---
+++ DecisionViewpointsTests/DecisionViewpointsEventHandlerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DecisionViewpoints;
+
+namespace DecisionViewpointsTests
+{
+    [TestClass]
+    public class DecisionViewpointsEventHandlerTests
+    {
+        [TestMethod]
+        public void Connect()
+        {
+            var eventHandler = new DecisionViewpoints.EventHandler();
+
+            Assert.AreEqual("connected", eventHandler.Connect());
+        }
+    }
+}
