de1d740c44153eee7fd354f77563ab07b9704bba
MarkHoekstra_cp <SND\MarkHoekstra_cp>
2014-04-17 17:53:16 +0000

    added create project structure unit test (TFS Integration from 'addin.visualstudio.com (VC)' Id: 44;CreationDate: 2013-02-25 17:09:15 UTC;)((Edited by spyrosikmd@gmail.com))

--- DecisionViewpointsTests/DecisionViewpointsAddinEventHandlerTests.cs
+++ DecisionViewpointsTests/DecisionViewpointsAddinEventHandlerTests.cs
@@ -30,7 +30,23 @@ namespace DecisionViewpointsTests
         [TestMethod]
         public void CreateProjectStructure()
         {
-            
+            var mainApp = new MainApplication();
+            var repository = new Repository();
+            var filename =
+                "F:\\DecisionViewpoints\\ViewpointsAddIn\\src\\DecisionViewpointsTests\\DecisionViewUnitTestsProject.eap";
+            Assert.IsTrue(repository.OpenFile(filename));
+            mainApp.EA_MenuClick(repository, "TreeView", "-&DecisionVS", "Create Decision &Views");
+            Package root = repository.Models.GetAt(0);
+            Package view = root.Packages.GetAt(0);
+            Assert.AreEqual("Decision Relationship View", view.Name);
+            Diagram diagram = view.Diagrams.GetAt(0);
+            Assert.AreEqual("Decision Relationship View", diagram.Name);
+            // Delete whatever we added so the file is clean and close the file.
+            foreach (Package p in root.Packages)
+            {
+                root.Packages.DeleteAt(0, true);   
+            }
+            repository.CloseFile();
         }
     }
 }
