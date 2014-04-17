b464ae41dc742644af09176d268d80c020f91ef1
MarkHoekstra_cp <SND\MarkHoekstra_cp>
2014-04-17 17:48:59 +0000

    fixed project solution file (TFS Integration from 'addin.visualstudio.com (VC)' Id: 27;CreationDate: 2013-02-20 15:02:11 UTC;)((Edited by spyrosikmd@gmail.com))

--- DecisionViewpoints/Properties/AssemblyInfo.cs
+++ DecisionViewpoints/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TSDAD")]
+[assembly: AssemblyTitle("DecisionViewpoints")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TSDAD")]
+[assembly: AssemblyProduct("DecisionViewpoints")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
