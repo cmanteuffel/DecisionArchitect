588a47afbde1e6e730b9a89202f3304a61844155
MarkHoekstra_cp <SND\MarkHoekstra_cp>
2014-04-17 17:48:24 +0000

    rename folder (TFS Integration from 'addin.visualstudio.com (VC)' Id: 23;CreationDate: 2013-02-20 14:45:02 UTC;)((Edited by spyrosikmd@gmail.com))

---
+++ TSDAD/EventHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using EA;
+using System.Windows.Forms;
+
+namespace TSDAD
+{
+    /// <summary>
+    /// This class is responsible for handling all the EA events delegated
+    /// from MainApplication entry point.
+    /// </summary>
+    public class EventHandler
+    {
+        public string Connect()
+        {
+            return "connected";
+        }
+
+        public bool OnPreNewElement(Repository repository, EventProperties info)
+        {
+            MessageBox.Show("to be added");
+            return true;
+        }
+
+        public bool OnPreNewConnector(Repository repository, EventProperties info)
+        {
+            Element client = repository.GetElementByID(info.Get(3).Value);
+            MessageBox.Show(String.Format("The client name is {0}", client.Name));
+            return true;
+        }
+
+        public void Disconnect()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
---
+++ TSDAD/MainApplication.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using EA;
+
+namespace TSDAD
+{
+    /// <summary>
+    /// This is the main entry point of the add-in.
+    /// All the events received in this class are delegated.
+    /// </summary>
+    public class MainApplication
+    {
+        private readonly EventHandler _eventHandler;
+
+        /// <summary>
+        /// The default constructor. An instance of EventHandler is created.
+        /// </summary>
+        public MainApplication()
+        {
+            _eventHandler = new EventHandler();
+        }
+
+        /// <summary>
+        /// Called Before EA starts to check Add-In Exists
+        /// Nothing is done here.
+        /// This operation needs to exists for the addin to work
+        /// </summary>
+        /// <param name="repository">the EA repository</param>
+        /// <returns>connected</returns>
+        public String EA_Connect(Repository repository)
+        {
+            return _eventHandler.Connect();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="repository">the EA repository</param>
+        /// <param name="info">contains properties of the element to be created</param>
+        /// <returns>true to permit the creation of the element, false to deny</returns>
+        public bool EA_OnPreNewElement(Repository repository, EventProperties info)
+        {
+            return _eventHandler.OnPreNewElement(repository, info);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="repository"></param>
+        /// <param name="info"></param>
+        /// <returns>true to permit the creation of the element, false to deny</returns>
+        public bool EA_OnPreNewConnector(Repository repository, EventProperties info)
+        {
+            return _eventHandler.OnPreNewConnector(repository, info);
+        }
+
+        /// <summary>
+        /// EA calls this operation when it exists. It is used to
+        /// do some cleanup work.
+        /// </summary>
+        public void EA_Disconnect() 
+        { 
+            _eventHandler.Disconnect();
+        }
+    }
+}
